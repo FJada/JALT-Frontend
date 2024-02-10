df914795deb8ac71f75cc0e3c2ea46b4d4fb9181
THERemyratatouille <lm4395@nyu.edu>
2024-02-10 14:31:47 -0500

    adding axios to users/account tab

--- jalt-frontend/src/Components/Users/Users.jsx
+++ jalt-frontend/src/Components/Users/Users.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './users.css'; 
+import axios from 'axios';
 
 function Users({ username }) {
+
+  const [error, setError] = useState('Initial Message');
+  const [users, setUsers] = useState([]);
+
+  useEffect(
+    () => {
+      axios.get('http://127.0.0.1:8000/users')
+      .then((response)=>{
+        const usersObject = response.data.Data;
+        const keys = Object.keys(usersObject);
+        const usersArray = keys.map((key) => usersObject[key]);
+        setUsers(usersArray);
+      })
+      .catch(() =>{setError('Something went wrong'); });
+
+    },
+    [],
+  );
   return (
     <div className="text">
       <h1>Hello, {username}</h1>
       <p>This is your account page.</p>
+      {error && (<div className='error-message'>
+        {error}
+        </div> 
+        )}
+      {users.map((user) => (
+        <div className='user-container'>
+          <h2>{user.username}</h2>
+          </div>
+
+      ))}
+      
     </div>
   );
 }
