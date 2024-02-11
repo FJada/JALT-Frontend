95b1e2a9a02bb45e8e763dfd0d63b22747bb6a45
Tanvi Saini <ts4175@nyu.edu>
2024-02-11 11:47:59 -0500

    buses pathway

--- jalt-frontend/src/App.js
+++ jalt-frontend/src/App.js
@@ -10,14 +10,16 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Trains from './Components/Trains';
 import Users from './Components/Users';
+import Buses from './Components/Buses';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path = "" element ={<h1 className="text">Home</h1>}/>
+        <Route path ="" element ={<h1 className="text">Home</h1>}/>
         <Route path="trains" element={<Trains />} />
+        <Route path="trains" element={<Buses />} />
         <Route path="users" element={<Users username="y/n" />} />
       </Routes>
     </BrowserRouter>
---
+++ jalt-frontend/src/Components/Buses/Buses.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './buses.css'; 
+
+function Buses() {
+  return (
+    <div className="text">
+      <h1>Buses</h1>
+    </div>
+  );
+}
+
+export default Buses;
