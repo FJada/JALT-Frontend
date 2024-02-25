8c909e81c13ef5fe359fccb9b35f0950a52c4a5e
THERemyratatouille <lm4395@nyu.edu>
2024-02-25 17:08:09 -0500

    adding add user button functionality to frontend

--- jalt-frontend/src/Components/Users/Users.jsx
+++ jalt-frontend/src/Components/Users/Users.jsx
@@ -6,9 +6,19 @@ function Users({ username }) {
 
   const [error, setError] = useState('Below is our list of users fetched from our API Server:');
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [newUserData, setNewUserData] = useState({
+    username: '',
+    account_id: '',
+  });
 
-  useEffect(
-    () => {
+
+  useEffect(()=>{
+    fetchUsers();
+  }, []);
+
+
+  const fetchUsers = () => {
       axios.get('http://127.0.0.1:8000/users')
       .then((response)=>{
         const usersObject = response.data.Data;
@@ -18,9 +28,33 @@ function Users({ username }) {
       }) // retrieves users
       .catch(() =>{setError('Something went wrong'); });
 
-    },
-    [],
-  );
+    };
+
+  const addUser = () => {
+      setIsLoading(true);
+      axios.post('http://127.0.0.1:8000/add_user', {
+        username: newUserData.username,
+        account_id: newUserData.account_id,
+      })
+        .then(() => {
+          // Once user is added successfully, fetch updated user list
+          fetchUsers();
+          setNewUserData({ username: '', account_id: '' });
+        })
+        .catch(() => {
+          setError('Failed to add user');
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    };
+  
+  const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setNewUserData({ ...newUserData, [name]: value });
+    };
+  
+
   return (
     <div className="text">
       <h1>Hello, {username}</h1>
@@ -29,6 +63,30 @@ function Users({ username }) {
         {error}
         </div> 
         )}
+    
+    <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          name="username"
+          value={newUserData.username}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label>Account ID:</label>
+        <input
+          type="text"
+          name="account_id"
+          value={newUserData.account_id}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <button onClick={addUser} disabled={isLoading}>
+        {isLoading ? 'Adding User...' : 'Add User'}
+      </button>
+
       {users.map((user) => (
         <div className='user-container'>
           <h2>{user.username}</h2>
