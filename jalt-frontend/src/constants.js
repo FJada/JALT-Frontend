c1cbc70c7049e7f1c5aa222f164f233a6b5c52b4
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2024-02-26 18:47:08 -0500

    env

--- jalt-frontend/src/constants.js
+++ jalt-frontend/src/constants.js
@@ -1,1 +1,4 @@
-export const BACKEND_URL = 'http://localhost:8000';
+//REACT_APP_URL_PRE = "http://127.0.0.1:8000"; //<- in order for it to run on my stinky windows laptop, ignore it
+export const BACKEND_URL = process.env.REACT_APP_URL_PRE;
+
+//old code: export const BACKEND_URL = 'http://localhost:8000';
