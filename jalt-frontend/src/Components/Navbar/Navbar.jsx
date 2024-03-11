4ec1192c28b9108ef20dbfc4d0d0c5b231065a10
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2024-03-10 22:36:01 -0400

    added mui/changed navbar

--- jalt-frontend/src/Components/Navbar/Navbar.jsx
+++ jalt-frontend/src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import classes from '../../global.module.css'
+import { AppBar, Toolbar, Typography, Link } from '@mui/material';
 
 const PAGES = [
   { label: 'Home', destination: '/Home' },
   { label: 'Map', destination: '/Map' },
-  {label:'Buses', destination:'/Buses'},
+  { label: 'Buses', destination: '/Buses' },
   { label: 'Trains', destination: '/Trains' },
   { label: 'Account', destination: '/Users' },
   { label: 'Log-in', destination: '/Log-in' }
-
-
 ];
+
+const navLinkStyle = {
+  marginRight: '1rem', // Adjust spacing between links
+  color: 'inherit', // Inherit text color from parent
+  textDecoration: 'none', // Remove default underline
+};
 
 function NavLink({ page }) {
   const { label, destination } = page;
   return (
-    <li>
-      <Link to={destination}>{label}</Link>
-    </li>
+    <Link href={destination} style={navLinkStyle}>
+      {label}
+    </Link>
   );
 }
+
 NavLink.propTypes = {
   page: PropTypes.shape({
     label: PropTypes.string.isRequired,
@@ -31,11 +35,18 @@ NavLink.propTypes = {
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        {PAGES.map((page) => <NavLink key={page.destination} page={page} />)}
-      </ul>
-    </nav>
+    <AppBar position="static" style={{ height: '90px' }}> {/* Adjust height here */}
+      <Toolbar>
+        <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
+          Navbar
+        </Typography>
+        <nav>
+          {PAGES.map((page) => (
+            <NavLink key={page.destination} page={page} />
+          ))}
+        </nav>
+      </Toolbar>
+    </AppBar>
   );
 }
 
