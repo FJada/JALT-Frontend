191ee0237a278ea51ace151301f899765bfebae4
THERemyratatouille <lm4395@nyu.edu>
2024-04-29 03:35:04 -0400

    fixing import

--- jalt-frontend/src/Components/Signup-Login/Forgot/Forgot.jsx
+++ jalt-frontend/src/Components/Signup-Login/Forgot/Forgot.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import classes from '../../global.module.css'
+import classes from '../../../global.module.css'
 import axios from 'axios';
 
 function Forgot(){
