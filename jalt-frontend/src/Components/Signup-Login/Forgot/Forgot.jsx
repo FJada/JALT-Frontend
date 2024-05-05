1913e7c1c5dc130804ec6c05cd16be49bd52e89f
FJada <124617739+FJada@users.noreply.github.com>
2024-05-05 04:21:33 -0400

    added forgot my password feature

--- jalt-frontend/src/Components/Signup-Login/Forgot/Forgot.jsx
+++ jalt-frontend/src/Components/Signup-Login/Forgot/Forgot.jsx
@@ -1,15 +1,49 @@
-import React, {useState, useEffect} from 'react';
-import classes from '../../../global.module.css'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import classes from '../../../global.module.css';
 import axios from 'axios';
 
-function Forgot(){
+function Forgot() {
+    const [username, setUsername] = useState('');
+    const [userData, setUserData] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
+    const handleGetUser = () => {
+        axios.get(`http://127.0.0.1:8000/users/${username}`)
+            .then(response => {
+                setUserData(response.data.Data);
+                setErrorMessage('');
+            })
+            .catch(error => {
+                setErrorMessage(error.response.data.message);
+                setUserData(null);
+            });
+    };
 
-    return( <div className={classes.text}>
-        <div className={classes.title}>Forgot Password?</div>
-        </div>);
+    return (
+        <div className={classes.text}>
+            <div className={classes.title}>Forgot Password?</div>
+            <input
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                placeholder="Enter username"
+            />
+            <button className={classes.btn} onClick={handleGetUser}>Retrieve User Details</button>
+            {errorMessage && <p className={classes.error}>{errorMessage}</p>}
+            {userData && (
+                <div>
+                    <p>Username: {userData.username}</p>
+                    <p>Password: {userData.password}</p>
+                </div>
+            )}
+            <div>
+        <span></span>
+        <Link to="/Login">Login</Link>
+      </div>
 
-
+        </div>
+    );
 }
 
 export default Forgot;
